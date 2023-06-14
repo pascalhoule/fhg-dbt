445873cd274d91415a5391628ea17daf9e86ed53
megan.anzai@financialhorizons.com <megan.anzai@financialhorizons.com>
2023-06-14 03:49:08 +0000

    apply raw cdc to transactions

--- models/clean/investment/transactions_clean_investment.sql
+++ models/clean/investment/transactions_clean_investment.sql
@@ -1,22 +1,79 @@
- {{  config(alias='transactions', database='clean', schema='investment', materialized='incremental')  }} 
+ {{  config(alias='transactions', database='clean', schema='investment')  }} 
 
 
-SELECT * 
-  ,datalake_timestamp as _infx_loaded_ts_utc  
-  
-            ,datalake_timestamp as _infx_active_from_ts_utc 
-  
-            ,case when datalake_end_ts = '3000-01-01 00:00:00.000' then '9999-12-31 23:59:59' else datalake_end_ts end as _infx_active_to_ts_utc  
-  
-            ,case when datalake_end_ts = '3000-01-01 00:00:00.000' then true else false end as _infx_is_active 
-  
+
+with 
+clean_trans as 
+(select
+code 
+,fundaccount_code
+,amount
+,average_cost
+,beartempcode
+,trade_date
+,setlement_date
+,dealer_commission
+,currencyname
+,fundproduct_name
+,net_amount
+,paymentid
+,settlementamount
+,shares_units
+,share_balance_after 
+,unit_price
+,ext_type_code 
+,payment_status 
+,settlement_period
+,cancel_flag 
+,transaction_flag
+,datalake_timestamp 
+,'insert' as cdc_operation
+from {{ this }}
+),
+raw_delta as 
+(
+    select 
+ code 
+,fundaccount_code
+,amount
+,average_cost
+,beartempcode
+,trade_date
+,setlement_date
+,dealer_commission
+,currencyname
+,fundproduct_name
+,net_amount
+,paymentid
+,settlementamount
+,shares_units
+,share_balance_after 
+,unit_price
+,ext_type_code 
+,payment_status 
+,settlement_period
+,cancel_flag 
+,transaction_flag
+,datalake_timestamp 
+,cdc_operation 
+    from {{ source ('investment_raw', 'transactions')  }}
+    where datalake_timestamp > (select max(clean_trans.datalake_timestamp) from clean_trans)
+    
+),
+uni as (
+select *
+from clean_trans
+union all
+select *
+from raw_delta
+), add_rn as (
+
+select *,
+row_number() over( partition by code order by datalake_timestamp desc  ) as rn
+from uni
+)
+select * exclude(rn) from add_rn
+where rn = 1
+and cdc_operation <> 'delete'
 
 
-from {{ source ('investment', 'transactions')  }}
-
-{% if is_incremental() %}
-
-  -- this filter will only be applied on an incremental run
-  where _infx_loaded_ts_utc > (select max(_infx_loaded_ts_utc) from {{ this }})
-
-{% endif %}
