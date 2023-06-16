13ab10a78c8cc11034263f61484dfa41ed1ef483
megan.anzai@financialhorizons.com <megan.anzai@financialhorizons.com>
2023-06-16 04:41:24 +0000

    FHG-315-client status deceased

--- models/clean/insurance/clients_clean_insurance.sql
+++ models/clean/insurance/clients_clean_insurance.sql
@@ -33,7 +33,7 @@ SELECT
 ,c.datalake_timestamp as _infx_active_from_ts_utc 
 ,case when c.datalake_end_ts = '3000-01-01 00:00:00.000' then '9999-12-31 23:59:59' else c.datalake_end_ts end as _infx_active_to_ts_utc 
 ,case when c.datalake_end_ts = '3000-01-01 00:00:00.000' then true else false end as _infx_is_active
-
+,c.status
 FROM {{ source('insurance','clients') }} c
 -- inner join {{ source('insurance','state') }} s on s.code = rtrim(c.state_code) -- to be deleted (discuss with FHG)
 where c.status < 100
