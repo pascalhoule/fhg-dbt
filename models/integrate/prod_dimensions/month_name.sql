507eb3d90ba047c2f90eb27a17082fcec8c1c6c0
megan.anzai@financialhorizons.com <megan.anzai@financialhorizons.com>
2023-09-29 12:06:14 +0000

    creates table of month names

---
+++ models/integrate/prod_dimensions/month_name.sql
@@ -0,0 +1,52 @@
+{{
+    config (
+        materialized="table",
+        alias='month_name', 
+        database='integrate', 
+        schema='prod_dimensions'
+    )
+}}
+
+WITH source_data AS (
+    SELECT
+        'Jan' AS MONTH_NAME_EN_SHORT,
+        'January' AS MONTH_NAME_EN,
+        'janv.' AS MONTH_NAME_FR_SHORT,
+        'janvier' AS MONTH_NAME_FR
+    UNION ALL
+    SELECT
+        'Feb', 'February', 'févr.', 'février'
+    UNION ALL
+    SELECT
+        'Mar', 'March', 'mars', 'mars'
+    UNION ALL
+    SELECT
+        'Apr', 'April', 'avr.', 'avril'
+    UNION ALL
+    SELECT
+         'May', 'May', 'mai', 'mai'
+    UNION ALL
+    SELECT
+         'Jun', 'June', 'juin', 'juin'
+    UNION ALL
+    SELECT
+         'Jul', 'July', 'juil.', 'juillet'
+    UNION ALL
+    SELECT
+         'Aug', 'August', 'août', 'août'
+    UNION ALL
+    SELECT
+         'Sep', 'September', 'sept.','septembre'
+    UNION ALL
+    SELECT     
+         'Oct', 'October', 'oct.', 'octobre'
+    UNION ALL
+    SELECT
+         'Nov', 'November', 'nov.', 'novembre'
+    UNION ALL
+    SELECT
+         'Dec', 'December', 'déc.', 'décembre'
+)
+
+SELECT *
+FROM source_data
