48eda6bc43a8d41d701fa99423a4546f25d37c52
megan.anzai@financialhorizons.com <megan.anzai@financialhorizons.com>
2024-04-03 19:46:52 +0000

    Investment data needed for LOI

---
+++ models/clean/investment/branches_vc_clean_investment.sql
@@ -0,0 +1,14 @@
+{{  config(alias='branches_vc', database='clean', schema='investment')  }} 
+
+SELECT * 
+  ,null as _infx_loaded_ts_utc  
+  
+            ,null as _infx_active_from_ts_utc 
+  
+            ,'9999-12-31 23:59:59' as _infx_active_to_ts_utc  
+  
+            ,null as _infx_is_active 
+  
+
+
+from {{ source ('investment_curated', 'branches_vc')  }}
