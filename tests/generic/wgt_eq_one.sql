ca0840d97783f4ea72cdf7dcc2b834fcf8b79e49
megan.anzai@financialhorizons.com <megan.anzai@financialhorizons.com>
2024-04-20 11:12:33 +0000

    Generic test wgt_eq_one

---
+++ tests/generic/wgt_eq_one.sql
@@ -0,0 +1,13 @@
+{% test wgt_eq_one(model, column_name, col_measure_to_test) %}
+
+SELECT
+    {{ column_name }},
+    ROUND(SUM( {{ col_measure_to_test }} ), 3) AS TOT_WGT
+FROM
+    {{ model }}
+GROUP BY
+    1
+HAVING
+    TOT_WGT <> 1
+
+{% endtest %}
