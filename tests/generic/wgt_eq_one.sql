579573f4dc618bab5937c96ab0aec95086f7a59e
megan.anzai@financialhorizons.com <megan.anzai@financialhorizons.com>
2024-04-24 18:12:38 +0000

    Added comments to test files

--- tests/generic/wgt_eq_one.sql
+++ tests/generic/wgt_eq_one.sql
@@ -1,5 +1,7 @@
 {% test wgt_eq_one(model, column_name, col_measure_to_test) %}
 
+{{ config(severity = 'warn') }} --This sets the default severity if it is not set in the .yml file
+
 SELECT
     {{ column_name }},
     ROUND(SUM( {{ col_measure_to_test }} ), 3) AS TOT_WGT
