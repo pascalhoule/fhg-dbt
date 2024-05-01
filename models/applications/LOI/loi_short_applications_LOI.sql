4b69c4e2a798ba038e0677d76898f1b4a5dbf2fc
megan.anzai@financialhorizons.com <megan.anzai@financialhorizons.com>
2024-05-01 17:43:34 +0000

    corrected LOI_SHORT_APPLICATIONS

--- models/applications/LOI/loi_short_applications_LOI.sql
+++ models/applications/LOI/loi_short_applications_LOI.sql
@@ -46,17 +46,6 @@ WHERE
 
 
 
-FROM
-{{ ref ('fundaccount_applications_investment')  }} fa_ods
-LEFT JOIN {{ ref ('registration_applications_investment')  }} r_ods on r_ods.code = fa_ods.registration_code
-LEFT JOIN {{ ref ('fundaccount_vc_applications_investment')  }} fa_cur on fa_cur.fundaccountcode = fa_ods.code
-LEFT JOIN {{ ref ('fundproducts_vc_applications_investment')  }} fp_cur on fp_cur.fundproductcode = fa_cur.fundproduct_code
-LEFT JOIN {{ ref ('sponsors_vc_applications_investment')  }} s_cur on s_cur.sponsorid = fp_cur.sponsorid
-LEFT JOIN {{ ref ('sponsor_applications_investment')  }} s_ods on s_ods.code = s_cur.code
-
-
-
-
 
 
 
