41dde2c78b612bb9b919fcd17828a8dfa1f991b8
megan.anzai@financialhorizons.com <megan.anzai@financialhorizons.com>
2024-05-06 17:58:59 +0000

    policy_fh for the daily app report

---
+++ models/report/daily_apps/policy_fh_report_daily_apps.sql
@@ -0,0 +1,43 @@
+{{			
+    config (			
+        materialized="view",			
+        alias='policy_fh', 			
+        database='report', 			
+        schema='daily_apps',
+        tags=["daily_apps"]			
+    )			
+}}
+
+    SELECT
+        FH_POLICYCATEGORY,
+        POLICYCODE,
+        FH_SERVICINGAGTCODE,
+        FH_SERVICINGAGTSPLIT,
+        FH_COMMISSIONINGAGTCODE,
+        FH_COMMISSIONINGAGTSPLIT,
+        FH_CARRIERENG,
+        FH_CARRIERFR,
+        POLICYNUMBER,
+        PLANID,
+        FH_PLANTYPE,
+        FH_PLANNAMEENG,
+        FH_PLANNAMEFR,
+        PREMIUMAMOUNT,
+        CREATEDDATE,
+        FH_STATUSCODE,
+        FH_STATUSNAMEENG,
+        FH_STATUSNAMEFR,
+        FH_STATUSCATEGORY,
+        APPCOUNT,
+        FH_FYCSERVAMT,
+        FH_FYCCOMMAMT,
+        ISSUEPROVINCE,
+        FH_APPSOURCE,
+        FH_APPTYPE,
+        FH_SETTLEMENTDATE,
+        FH_STARTDATE,
+        FH_PREMIUM,
+        FH_PREM_COMMWGT,
+        FH_PREM_SERVWGT
+    FROM
+        {{ ref('policy_fh_report_insurance') }}
