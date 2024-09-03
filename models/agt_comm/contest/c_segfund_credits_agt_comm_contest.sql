ecb0ecc3b81f836edc21bcc477874fc0021061ca
megan.anzai@financialhorizons.com <megan.anzai@financialhorizons.com>
2024-09-03 15:22:05 +0000

    moving b_life _credits to correct folder and addeing c_segfund credits

---
+++ models/agt_comm/contest/c_segfund_credits_agt_comm_contest.sql
@@ -0,0 +1,31 @@
+{{ config( alias='c_segfund_credits', database='agt_comm', schema='contest', materialized = "view" ) }} 
+
+SELECT
+    BA.USERDEFINED2 AS UD2,
+    BA.AGENTCODE,
+    SUM(T.AMOUNT * TT.DBSIGN) AS SEG_SALES_AMOUNT,
+    CASE
+        WHEN SUM(T.AMOUNT * TT.DBSIGN) < 0 THEN SUM(T.AMOUNT * TT.DBSIGN) * 0.03
+        ELSE 0
+    END AS SEG_SALES_CREDIT
+FROM
+    {{ ref('__base_transactions_for_contest_agt_comm_contest') }} AS T
+JOIN
+    {{ ref('brokeradvanced_vc_agt_comm_insurance') }} AS BA
+    ON T.REPID = BA.AGENTCODE
+INNER JOIN
+    {{ ref('__base_segfund_transtypes_agt_comm_contest') }} AS TT
+    ON T.TRANSACTIONTYPECODE = TT.TRANSACTIONTYPECODE
+WHERE
+    TRADEDATE >= (
+        SELECT MAX(SEG_FUNDS_STARTDATE)
+        FROM
+            {{ source('contest', 'date_ranges') }}
+    )
+    AND TRADEDATE <= (
+        SELECT MAX(SEG_FUNDS_ENDDATE)
+        FROM
+            {{ source('contest', 'date_ranges') }}
+    )
+GROUP BY
+    1, 2
