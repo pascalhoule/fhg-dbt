b7af440f6d09e73aeabb12906a67ff3ee14e1794
megan.anzai@financialhorizons.com <megan.anzai@financialhorizons.com>
2024-10-11 11:32:11 +0000

    materializing __COMM_WGTS as a table

--- models/normalize/insurance/__COMM_WGTS_insurance.sql
+++ models/normalize/insurance/__COMM_WGTS_insurance.sql
@@ -1,4 +1,4 @@
-{{  config(alias='__comm_wgts', database='normalize', schema='insurance', tags=["policy_fh"])  }} 
+{{  config(alias='__comm_wgts', database='normalize', schema='insurance', tags=["policy_fh"], materialized="table")  }} 
 
     WITH FIN_TOTAL_PAID AS (
         SELECT
