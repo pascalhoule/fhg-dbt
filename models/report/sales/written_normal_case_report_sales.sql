b5f016f11e40ef52645d43686a866d48d167b38c
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2024-12-10 18:22:42 +0000

    adding written and paid queries to the sales database

---
+++ models/report/sales/written_normal_case_report_sales.sql
@@ -0,0 +1,26 @@
+{{			
+    config (			
+        materialized="view",			
+        alias='written_normal_case', 			
+        database='report', 			
+        schema='sales',
+        grants = {'ownership': ['FH_READER']},
+        tags=["sales", "large_case"]			
+    )			
+}}
+
+SELECT
+    POLICYNUMBER,
+    POLICYGROUPCODE,
+    FH_STARTDATE,
+    SUM(FH_FYCSERVAMT) AS WRITTEN_FYC
+FROM
+    {{ ref('policy_fh_report_insurance') }} 
+WHERE
+    FH_STARTDATE BETWEEN '2020-01-01'
+    AND CURRENT_DATE
+    AND FH_POLICYCATEGORY IN ('NEW POLICY', 'NEW RIDER')
+GROUP BY
+    1, 2, 3
+    HAVING
+    WRITTEN_FYC <= 25000
