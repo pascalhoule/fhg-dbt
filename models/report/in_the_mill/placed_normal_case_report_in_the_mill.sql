1343369fd3c8ba4a0dd716296f30e1d58180b667
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-01-10 20:36:29 +0000

    placed large and placed normal case queries for daily app in the mill and sales reports

---
+++ models/report/in_the_mill/placed_normal_case_report_in_the_mill.sql
@@ -0,0 +1,25 @@
+{{			
+    config (			
+        materialized="view",			
+        alias='placed_normal_case', 			
+        database='report', 			
+        schema='in_the_mill',
+        tags=["in_the_mill", "large case"]			
+    )			
+}}
+
+SELECT
+    POLICYNUMBER,
+    POLICYGROUPCODE,
+    FH_PLACEDDATE,
+    SUM(FH_FYCPLACED) AS PLACED_FYC
+FROM
+    {{ ref('policy_fh_report_insurance') }} 
+WHERE
+    FH_PLACEDDATE BETWEEN '2020-01-01'
+    AND CURRENT_DATE
+    AND FH_POLICYCATEGORY IN ('NEW POLICY', 'NEW RIDER')
+GROUP BY
+    1, 2, 3
+HAVING
+    PLACED_FYC < 25000
