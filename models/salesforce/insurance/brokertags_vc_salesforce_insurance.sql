df1eef320de48b71fc2d13a8270ad9c50c74c5c4
Arzoo Mor <Arzoo.Mor@Financialhorizons.ca>
2025-01-23 15:16:00 +0000

    grants permissions

--- models/salesforce/insurance/brokertags_vc_salesforce_insurance.sql
+++ models/salesforce/insurance/brokertags_vc_salesforce_insurance.sql
@@ -1,7 +1,8 @@
 {{  config(
     alias='brokertags_vc', 
     database='salesforce', 
-    schema='insurance')  }}
+    schema='insurance',
+    grants = {'ownership': ['FH_READER']})  }}
 
 SELECT *
 FROM {{ ref('brokertags_vc_clean_insurance') }}
