dd186e7b4ad805b04e94434e33afaa6c35a45a8d
Arzoo Mor <Arzoo.Mor@Financialhorizons.ca>
2025-01-23 15:05:52 +0000

    Grants permissions

--- models/salesforce/insurance/brokercarrierdebt_vc_salesforce_insurance.sql
+++ models/salesforce/insurance/brokercarrierdebt_vc_salesforce_insurance.sql
@@ -1,7 +1,8 @@
 {{  config(
     alias='brokercarrierdebt_vc', 
     database='salesforce', 
-    schema='insurance')  }}
+    schema='insurance',
+    grants = {'ownership': ['FH_READER']}) }}
 
 SELECT *
 FROM {{ ref('brokercarrierdebt_vc_clean_insurance') }}
