802be01a313deba67c6d1cc6211d9b524aacc34e
Arzoo Mor <Arzoo.Mor@Financialhorizons.ca>
2025-01-24 21:59:09 +0000

    created GA_provinciallicences

---
+++ models/salesforce/exports/GA_provinciallicences_salesforce_exports.sql
@@ -0,0 +1,19 @@
+{{ config(
+    alias='GA_provinciallicences', 
+    database='salesforce', 
+    schema='exports',
+   materialized="view",
+   grants = {'ownership': ['FH_READER']}) }}
+
+With Temp As (
+    Select Pl.*
+    From
+        {{ ref('provinciallicences_V_salesforce_exports') }} As Pl
+    Where Exists (
+        Select 1
+        From {{ ref('broker_V_salesforce_exports') }} As Bt
+        Where Bt.Agentcode = Pl.Agentcode
+    )
+)
+
+Select * From Temp
