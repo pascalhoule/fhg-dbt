8e3864ad8a9f6b8ca5900f9daa505a14d5d4486a
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-02-06 21:06:34 +0000

    adding segment to hierarchy_fh

--- models/integrate/insurance/hierarchy_fh_integrate_insurance.sql
+++ models/integrate/insurance/hierarchy_fh_integrate_insurance.sql
@@ -5,5 +5,9 @@
     schema='insurance')  
     }} 
 
-SELECT *
-FROM {{ ref('hierarchy_fh_normalize_insurance') }}
+SELECT
+    H.*,
+    M.NEW_SEGMENT AS SEGMENT
+FROM
+    {{ ref('hierarchy_fh_normalize_insurance') }} H
+    LEFT JOIN {{ source('dimensions', 'agent_firm_segment_map') }} M ON H.BROKERID = M.BROKERID
