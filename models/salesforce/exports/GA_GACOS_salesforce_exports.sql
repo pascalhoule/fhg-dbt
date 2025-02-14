d7bbae8f5ebb09f9955f4b0891af56a4f986de46
Arzoo Mor <Arzoo.Mor@Financialhorizons.ca>
2025-02-14 18:00:46 +0000

    updated GA_GACOS

--- models/salesforce/exports/GA_GACOS_salesforce_exports.sql
+++ models/salesforce/exports/GA_GACOS_salesforce_exports.sql
@@ -6,14 +6,18 @@
    grants = {'ownership': ['FH_READER']}) }}
 
 WITH Temp AS (
-    SELECT Bs.*
-    FROM
-        {{ ref('GACOS_salesforce_exports') }} AS Bs
-    WHERE EXISTS (
-        SELECT 1
-        FROM {{ ref('GA_accounts_global_salesforce_exports') }} AS Ag
-        WHERE Ag.Uid = Bs.Ga_uid
-    )
+    SELECT DISTINCT
+        Bc.Role,
+        Bc.Employeename,
+        TRIM(Ba.Userdefined2) AS Ga_uid,
+        CONCAT('uid_', TRIM(Ba.Userdefined2), '_', TRIM(Bc.Rolecode)) AS Uid
+    FROM {{ ref('brokercos_vc_salesforce_insurance') }} AS Bc
+    LEFT JOIN {{ ref('brokeradvanced_vc_salesforce_insurance') }} AS Ba
+        ON Bc.Agentcode = Ba.Agentcode
+    WHERE
+        LEFT(Ba.Userdefined2, 4) IN ('3162', '3268')
+        AND LENGTH(Ba.Userdefined2) < 10
+    ORDER BY TRIM(Ba.Userdefined2), TRIM(Bc.Role)
 )
 
 SELECT * FROM Temp
