7c8090fcfab744f0117615b43a99b5f0629100b8
Arzoo Mor <Arzoo.Mor@Financialhorizons.ca>
2025-02-25 18:37:21 +0000

    GACOS updated in salesforce

--- models/salesforce/exports/GACOS_salesforce_exports.sql
+++ models/salesforce/exports/GACOS_salesforce_exports.sql
@@ -5,16 +5,24 @@
    materialized="view",
    grants = {'ownership': ['FH_READER']}) }}
 
-SELECT DISTINCT
-    BC.ROLE,
-    BC.EMPLOYEENAME,
-    '' AS EMPLOYEEEMAIL,
-    TRIM(BA.USERDEFINED2) AS GA_UID,
-    CONCAT('uid_', TRIM(BA.USERDEFINED2), '_', TRIM(BC.ROLECODE)) AS UID
-FROM {{ ref('brokercos_vc_salesforce_insurance') }} AS BC
-LEFT JOIN {{ ref('brokeradvanced_vc_salesforce_insurance') }} AS BA
-    ON BC.AGENTCODE = BA.AGENTCODE
-WHERE
-    LEFT(BA.USERDEFINED2, 4) IN ('3162', '3268')
-    AND LENGTH(BA.USERDEFINED2) < 10
-ORDER BY TRIM(BA.USERDEFINED2), TRIM(BC.ROLE)
+SELECT
+    GA_UID,
+    UID,
+    ROLE,
+    EMPLOYEENAME
+FROM (
+    SELECT DISTINCT
+        BC.ROLE,
+        BC.EMPLOYEENAME,
+        TRIM(BA.USERDEFINED2) AS GA_UID,
+        CONCAT('uid_', TRIM(BA.USERDEFINED2), '_', TRIM(BC.ROLECODE)) AS UID,
+        ROW_NUMBER() OVER (PARTITION BY BC.ROLE, GA_UID, UID ORDER BY UID) AS R
+    FROM {{ ref('brokercos_vc_salesforce_insurance') }} AS BC
+    LEFT JOIN {{ ref('brokeradvanced_vc_salesforce_insurance') }} AS BA
+        ON BC.AGENTCODE = BA.AGENTCODE
+    WHERE
+        LEFT(BA.USERDEFINED2, 4) IN ('3162', '3268')
+        AND LENGTH(BA.USERDEFINED2) < 10
+) AS FILTEREDDATA
+WHERE R = 1
+ORDER BY GA_UID, ROLE
