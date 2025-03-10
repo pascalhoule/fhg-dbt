374043152ac3ccadc24e08608b17a365e3ca16c1
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-03-10 14:09:17 +0000

    adding carrier name to aua

--- models/report/segfund/aua_report_segfund.sql
+++ models/report/segfund/aua_report_segfund.sql
@@ -19,6 +19,7 @@ SELECT
     LAST_NAME,
     FUNDPRODUCT.FUNDPRODUCTCODE,
     FUNDPRODUCT.SPONSORID,
+    SPONSOR.NAME,
     FUNDPRODUCT.FUNDID,
     FUNDPRODUCT.NAME,
     FUNDPRODUCT.LOADTYPE,
@@ -29,7 +30,8 @@ FROM
     {{ ref('aua_vc_report_investment') }} AUA
     join {{ ref('representatives_vc_report_investment') }} REP ON AUA.REPCODE = REP.REPRESENTIATIVECODE
     join {{ ref('fundproducts_vc_report_investment') }} FUNDPRODUCT on AUA.FUNDPRODUCTCODE = FUNDPRODUCT.FUNDPRODUCTCODE
+    JOIN {{ ref('sponsor_vc_report_investment') }} SPONSOR ON SPONSOR.SPONSORID = FUNDPRODUCT.SPONSORID
 WHERE
     TRENDDATE::DATE >= '2025-01-01' 
 group by
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
