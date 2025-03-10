5a5ea9327bca1c159937e19b08a14c407f5b1e73
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-03-10 12:33:11 +0000

    removing extra fields from region_vc and branches_vc

--- models/normalize/investment/branches_vc_normalize_investment.sql
+++ models/normalize/investment/branches_vc_normalize_investment.sql
@@ -1,7 +1,10 @@
  {{  config(alias='branches_vc', database='normalize', schema='investment')  }} 
 
 
-SELECT * 
-  
-
+SELECT 
+    REGIONCODE,
+    SUBREGIONCODE,
+    CODE,
+    BRANCH_ID,
+    NAME
 FROM {{ ref ('branches_vc_clean_investment')  }}
