549799549dbed8d4bfd4eaa5022841ae41df7ed4
Arzoo Mor <Arzoo.Mor@Financialhorizons.ca>
2025-04-07 15:04:26 +0000

    updated history email tracking

--- models/applications/EmailTracking/history_brokeremailtracking_emailtracking_applications.sql
+++ models/applications/EmailTracking/history_brokeremailtracking_emailtracking_applications.sql
@@ -33,7 +33,7 @@ SELECT
     T.BUSINESS_EMAILADDRESS,
     '' as PERSON1_EMAILADDRESS,
     '' as PERSON2_EMAILADDRESS,
-    T.AGENTSTATUS --this is the agent status at the time the e-mail changed.
+    T.AGENTSTATUS, --this is the agent status at the time the e-mail changed.
     CURRENT_TIMESTAMP  
   FROM {{ ref('broker_businessemail_tracking') }}  T join BUS_TRACK on BUS_TRACK.agentcode = T.agentcode
    WHERE dbt_valid_to is null
@@ -47,7 +47,7 @@ SELECT
     '' as BUSINESS_EMAILADDRESS,
     PERSON1_EMAILADDRESS,
     '' as PERSON2_EMAILADDRESS,
-    T.AGENTSTATUS --this is the agent status at the time the e-mail changed.
+    T.AGENTSTATUS, --this is the agent status at the time the e-mail changed.
     CURRENT_TIMESTAMP  
    FROM {{ ref('broker_person1email_tracking') }}  T join PER1_TRACK on PER1_TRACK.agentcode = T.agentcode
     WHERE dbt_valid_to is null
@@ -60,7 +60,7 @@ UNION
     '' as BUSINESS_EMAILADDRESS,
     '' as PERSON1_EMAILADDRESS,
     PERSON2_EMAILADDRESS,
-    T.AGENTSTATUS --this is the agent status at the time the e-mail changed.
+    T.AGENTSTATUS, --this is the agent status at the time the e-mail changed.
     CURRENT_TIMESTAMP  --should this be when the e-mail changed OR when the table is created?
    FROM {{ ref('broker_person2email_tracking') }}  T join PER2_TRACK on PER2_TRACK.AGENTCODE = T.AGENTCODE
    WHERE dbt_valid_to is null
