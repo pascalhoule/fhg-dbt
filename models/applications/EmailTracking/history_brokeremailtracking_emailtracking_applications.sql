984175ae94ac69f7207b582e5407847eb5acd61e
Arzoo Mor <Arzoo.Mor@Financialhorizons.ca>
2025-04-09 19:04:00 +0000

    made changes in history broker email tracking

--- models/applications/EmailTracking/history_brokeremailtracking_emailtracking_applications.sql
+++ models/applications/EmailTracking/history_brokeremailtracking_emailtracking_applications.sql
@@ -31,10 +31,10 @@ SELECT
     T.AGENTCODE,
     T.BROKERID,
     T.BUSINESS_EMAILADDRESS,
-    '' as PERSON1_EMAILADDRESS,
-    '' as PERSON2_EMAILADDRESS,
+    Null as PERSON1_EMAILADDRESS,
+    Null as PERSON2_EMAILADDRESS,
     T.AGENTSTATUS, --this is the agent status at the time the e-mail changed.
-    dbt_valid_to as Email_Changed
+    dbt_valid_from as Email_Changed
   FROM {{ ref('broker_businessemail_tracking') }}  T join BUS_TRACK on BUS_TRACK.agentcode = T.agentcode
    WHERE dbt_valid_to is  not null
 
@@ -44,11 +44,11 @@ UNION
 SELECT
     T.AGENTCODE,
     T.BROKERID,
-    '' as BUSINESS_EMAILADDRESS,
+    Null as BUSINESS_EMAILADDRESS,
     PERSON1_EMAILADDRESS,
-    '' as PERSON2_EMAILADDRESS,
+    Null as PERSON2_EMAILADDRESS,
     T.AGENTSTATUS, --this is the agent status at the time the e-mail changed.
-    dbt_valid_to as Email_Changed
+    dbt_valid_from as Email_Changed
    FROM {{ ref('broker_person1email_tracking') }}  T join PER1_TRACK on PER1_TRACK.agentcode = T.agentcode
     WHERE dbt_valid_to is not null
  
@@ -57,10 +57,10 @@ UNION
   SELECT
     T.AGENTCODE,
     T.BROKERID,
-    '' as BUSINESS_EMAILADDRESS,
-    '' as PERSON1_EMAILADDRESS,
+    Null as BUSINESS_EMAILADDRESS,
+    Null as PERSON1_EMAILADDRESS,
     PERSON2_EMAILADDRESS,
     T.AGENTSTATUS, --this is the agent status at the time the e-mail changed.
-    dbt_valid_to as Email_Changed
+    dbt_valid_from as Email_Changed
    FROM {{ ref('broker_person2email_tracking') }}  T join PER2_TRACK on PER2_TRACK.AGENTCODE = T.AGENTCODE
    WHERE dbt_valid_to is not null
