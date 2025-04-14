00fb2a986807101b53594d4fa4da4281a004195b
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-04-14 00:42:18 +0000

    changing how the date range works to use in  incl_in_rpt field

--- models/agt_comm/contest/b_life_credits_agt_comm_contest.sql
+++ models/agt_comm/contest/b_life_credits_agt_comm_contest.sql
@@ -1,10 +1,19 @@
 {{ config( alias='b_life_credits', database='agt_comm', schema='contest', materialized = "view" ) }} 
 
-SELECT
-    BA.USERDEFINED2 AS UD2,
-    C.OWNERCODE AS AGENTCODE,
-    SUM(C.COMMISSIONAMOUNT) AS FYC_AMOUNT,
-    SUM(C.COMMISSIONAMOUNT) AS FYC_CREDITS
+WITH DATES AS (
+            SELECT
+                LIFE_CREDITS_STARTDATE,
+                LIFE_CREDITS_ENDDATE
+            FROM
+                {{ source('contest', 'date_ranges') }}
+            WHERE
+                INCL_IN_RPT = TRUE
+        )
+        SELECT
+            BA.USERDEFINED2 AS UD2,
+            C.OWNERCODE AS AGENTCODE,
+            SUM(C.COMMISSIONAMOUNT) AS FYC_AMOUNT,
+            SUM(C.COMMISSIONAMOUNT) AS FYC_CREDITS
 FROM
     {{ ref('brokeradvanced_vc_agt_comm_insurance') }} AS BA
 INNER JOIN
@@ -15,15 +24,17 @@ INNER JOIN
     ON BA.USERDEFINED2 = DIM.USERDEFINED2 AND BA.AGENTCODE = DIM.AGENTCODE
 WHERE
     C.PAIDDATE >= (
-        SELECT MAX(LIFE_CREDITS_STARTDATE)
-        FROM
-            {{ source('contest', 'date_ranges') }}
-    )
-    AND C.PAIDDATE <= (
-        SELECT MAX(LIFE_CREDITS_ENDDATE)
-        FROM
-            {{ source('contest', 'date_ranges') }}
-    )
+                SELECT
+                    LIFE_CREDITS_STARTDATE
+                FROM
+                    DATES
+            )
+            AND C.PAIDDATE <= (
+                SELECT
+                    LIFE_CREDITS_ENDDATE
+                FROM
+                    DATES
+            )
     AND PAIDBY = 'carrierdirect'
     AND TRXTYPE = 'FYC'
     AND PLANTYPE IN (
