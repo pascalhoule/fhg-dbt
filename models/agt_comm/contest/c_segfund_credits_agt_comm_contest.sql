4146661ac4bc4e525d4315647f6ffd9fd14d2940
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-04-28 20:15:46 +0000

    changing segfund code to add up production then calculate credits

--- models/agt_comm/contest/c_segfund_credits_agt_comm_contest.sql
+++ models/agt_comm/contest/c_segfund_credits_agt_comm_contest.sql
@@ -8,25 +8,22 @@ WITH DATES AS (
                 {{ source('contest', 'date_ranges') }}
             WHERE
                 INCL_IN_RPT = TRUE
-                )
-SELECT
-    BA.USERDEFINED2 AS UD2,
-    BA.AGENTCODE,
-    SUM(T.AMOUNT * TT.DBSIGN) AS SEG_SALES_AMOUNT,
-    CASE
-        WHEN SUM(T.AMOUNT * TT.DBSIGN) > 0 THEN SUM(T.AMOUNT * TT.DBSIGN) * 0.03
-        ELSE 0
-    END AS SEG_SALES_CREDIT
-FROM
+                ),
+     AMT AS (            
+            SELECT
+            BA.USERDEFINED2 AS UD2,
+            BA.AGENTCODE,
+            SUM(T.AMOUNT * TT.DBSIGN) AS SEG_SALES_AMOUNT,
+    FROM
     {{ ref('__base_transactions_for_contest_agt_comm_contest') }} AS T
-JOIN
+     JOIN
     {{ ref('a_dim_agt_comm_contest') }} AS BA
     ON T.REPID = BA.AGENTCODE
-INNER JOIN
+    INNER JOIN
     {{ ref('__base_segfund_transtypes_agt_comm_contest') }} AS TT
     ON T.TRANSACTIONTYPECODE = TT.TRANSACTIONTYPECODE
-WHERE
-    TRADEDATE >= (
+    WHERE
+        TRADEDATE >= (
                 SELECT
                     SEG_FUNDS_STARTDATE
                 FROM
@@ -37,6 +34,23 @@ WHERE
                     SEG_FUNDS_ENDDATE
                 FROM
                     DATES
-    )
+    ) 
 GROUP BY
-    1, 2
+    1, 2),
+
+
+ CREDITS AS
+    (SELECT UD2, SUM(SEG_SALES_AMOUNT) AS SALES,
+    CASE WHEN SUM(SEG_SALES_AMOUNT) < 0 THEN 0 ELSE 0.03*SUM(SEG_SALES_AMOUNT) END AS SEG_SALES_CREDITS
+    FROM AMT
+    GROUP BY ALL
+    )
+
+ SELECT 
+    A.UD2, 
+    A.AGENTCODE, 
+    C.SALES AS SEG_SALES_AMOUNT, 
+    C.SEG_SALES_CREDITS
+ FROM CREDITS C JOIN AMT A on C.UD2 = A.UD2
+
+  
