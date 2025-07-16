1d04570ef20cb62817e36a666d335466cd3d0d7a
Arzoo Mor <Arzoo.Mor@Financialhorizons.ca>
2025-07-16 20:37:16 +0000

    change the source to new acdirect_policy source

--- models/integrate/insurance/__base_CL_pol_duplicates_integrate_insurance.sql
+++ models/integrate/insurance/__base_CL_pol_duplicates_integrate_insurance.sql
@@ -11,7 +11,7 @@
 SELECT DISTINCT
     FH.POLICYNUMBER,
     CL.CURRENT_CONTRACT_POLICY_NUMBER
-FROM {{ source("acdirect", "daily_insurance_ac_direct_agreement") }} AS CL
+FROM {{ source("acdirect_policy", "daily_insurance_AC_Direct_agreement_20250716") }} AS CL
 inner join
     {{ ref('policy_fh_integrate_insurance') }} AS FH
     on CL.CURRENT_CONTRACT_POLICY_NUMBER = FH.POLICYNUMBER
