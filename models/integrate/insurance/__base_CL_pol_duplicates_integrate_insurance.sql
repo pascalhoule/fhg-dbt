eb55559df69fdb18b07eeb238b73061a9b8d25e6
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-07-19 18:55:54 +0000

    changing sources

--- models/integrate/insurance/__base_CL_pol_duplicates_integrate_insurance.sql
+++ models/integrate/insurance/__base_CL_pol_duplicates_integrate_insurance.sql
@@ -4,14 +4,14 @@
         alias="__base_CL_pol_duplicates",
         database="integrate",
         schema="insurance",
-        grants={"ownership": ["FH_READER"]},
+        grants={"ownership": ["FH_READER"]}
     )
 }}
 
 SELECT DISTINCT
     FH.POLICYNUMBER,
     CL.CURRENT_CONTRACT_POLICY_NUMBER
-FROM {{ source("acdirect", "daily_insurance_ac_direct_agreement") }} AS CL
+FROM {{ source("acdirect_leo", "daily_insurance_ac_direct_agreement") }} AS CL
 inner join
     {{ ref('policy_fh_integrate_insurance') }} AS FH
     on CL.CURRENT_CONTRACT_POLICY_NUMBER = FH.POLICYNUMBER
