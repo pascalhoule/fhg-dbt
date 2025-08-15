6ff842e255a2535b5d4a5f3666995e888d4c13a5
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-08-15 20:41:54 +0000

    need to materialize integrate.prod_insurance.broker_fh_cl as a table and not a view

--- models/integrate/insurance/broker_fh_cl_integrate_insurance.sql
+++ models/integrate/insurance/broker_fh_cl_integrate_insurance.sql
@@ -1,6 +1,6 @@
 {{			
     config (			
-        materialized="view",			
+        materialized="table",			
         alias='broker_fh_cl', 			
         database='integrate', 			
         schema='insurance',
