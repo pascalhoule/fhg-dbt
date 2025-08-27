575ea9f3d8c169253ab3fca4638a96ca140cd8dd
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-08-27 15:04:20 +0000

    removing the infostrux timestamp fields

--- models/finance/insurance/brokeraddress_vc_finance_prod_insurance.sql
+++ models/finance/insurance/brokeraddress_vc_finance_prod_insurance.sql
@@ -5,5 +5,11 @@
     materialization = "view") }} 
 
 SELECT
-    *
+    AGENTCODE, 
+    TYPE, 
+    ADDRESS, 
+    CITY, 
+    PROVINCE, 
+    POSTAL_CODE, 
+    COUNTRY
 FROM {{ ref('brokeraddress_vc_clean_insurance') }}
