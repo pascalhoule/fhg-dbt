85e66fd154cd4c07258c2a16d293443758ce4d14
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-10-09 15:44:21 +0000

    changing the FH_SERVICINGAGTCODE to type VARCHAR

--- models/report/insurance/itm_calcs_FH_report_insurance.sql
+++ models/report/insurance/itm_calcs_FH_report_insurance.sql
@@ -3,4 +3,26 @@
     database = 'report', 
     schema = 'insurance') }} 
 
-    SELECT * from {{ ref('itm_calcs_FH_analyze_insurance') }}
+    SELECT 
+        FH_POLICYCATEGORY,
+        POLICYNUMBER,
+        POLICYCODE,
+        CAST(FH_SERVICINGAGTCODE AS VARCHAR(50)) AS FH_SERVICINGAGTCODE,
+        FH_STATUSCODE,
+        FH_STATUSNAMEENG,
+        FH_STATUSCATEGORY,
+        FH_STARTDATE,
+        CREATEDDATE,
+        CONTRACTDATE,
+        SETTLEMENTDATE,
+        EXPIRYDATE,
+        RENEWALDATE,
+        SENTTOICDATE,
+        MAILEDDATE,
+        FH_FINPOSTDATE,
+        ITM_END_DATE,
+        ITM,
+        DAYS_IN_STATUS
+    from {{ ref('itm_calcs_FH_analyze_insurance') }}
+
+   
