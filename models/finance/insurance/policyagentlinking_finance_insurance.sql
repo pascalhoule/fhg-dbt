b0ccead2e1dfe9494b0abe1f86e4f82d9e2decc1
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2025-12-12 20:04:33 +0000

    changing materialization to materialized in more views FHSNF-180

--- models/finance/insurance/policyagentlinking_finance_insurance.sql
+++ models/finance/insurance/policyagentlinking_finance_insurance.sql
@@ -1,4 +1,4 @@
-{{  config(alias='policyagentlinking', database='finance', schema='insurance', materialization = "view")  }} 
+{{  config(alias='policyagentlinking', database='finance', schema='insurance', materialized = "view")  }} 
 
 SELECT * 
   
