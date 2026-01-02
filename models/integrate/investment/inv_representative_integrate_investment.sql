eaa11a7b266e6bac909a8a003f34bf2884ec9078
Evelyn Wynen <Evelyn.Wynen@Financialhorizons.ca>
2026-01-02 16:39:35 +0000

    Fixing the spelling of representative in all tables from NORMALIZE forward.  The CLEAN database is not touched.

--- models/integrate/investment/inv_representative_integrate_investment.sql
+++ models/integrate/investment/inv_representative_integrate_investment.sql
@@ -6,7 +6,7 @@ rep.repid = ba.userdefined2 or ((try_cast(ba.userdefined2 as integer) > 9999999)
 phone.number as bus_phone,
 email.emailaddress as bus_email,
 concat( adr.address, ' ', adr.city, ' ',  adr.province, ' ',  adr.postalcode, ' ', adr.country ) as bus_address,
-rep.representiativecode, rep.insagentcode, rep.repid, rep.first_name, rep.last_name, rep.DOB, rep.branch_code, rep.sin, rep.repstatus
+rep.representativecode, rep.insagentcode, rep.repid, rep.first_name, rep.last_name, rep.DOB, rep.branch_code, rep.sin, rep.repstatus
 from {{ ref('representatives_vc_normalize_investment') }} rep
 left join {{ ref('brokeradvanced_vc_normalize_insurance') }} ba on ba.agentcode = rep.insagentcode
 left join {{ ref('representativeaddress_vc_normalize_investment') }} adr on rep.code = adr.representativecode and adr.type = 'business'
