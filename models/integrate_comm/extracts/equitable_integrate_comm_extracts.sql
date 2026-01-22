2646b47933d8672746910f9d7ff5017a44cb35e9
Pascal Houle <Pascal.Houle@financierehorizons.ca>
2026-01-22 14:15:35 +0000

    New view for Equitable in INTEGRATE_COMM

--- models/integrate_comm/extracts/equitable_integrate_comm_extracts.sql
+++ models/integrate_comm/extracts/equitable_integrate_comm_extracts.sql
@@ -7,20 +7,42 @@
 ) }} 
 
 SELECT
-    TRIM(MGA) AS MGA, 
-    DATE("GEN DATE") AS GEN_DATE,
-    TRIM(POLICY) AS POLICY,
-    TRIM(BRANCH) AS BRANCH,
-    TRIM("OWNER LAST NAME") AS OWNER_LAST_NAME,
-    TRIM("PLAN CODE") AS PLAN_CODE,
-    AGENT,
-    TRIM("AGENT NAME") AS AGENT_NAME,
-    TRIM(DESC) AS DESCRIPTION,
-    "COMP AMT" AS COMP_AMT,
-    "COMM SHARE" AS COMM_SHARE,
-    "COMM RATE" AS COMM_RATE,
-    "COMMISSIONABLE PREMIUM" AS COMMISSIONABLE_PREMIUM,
-    "POLICY COUNT" AS POLICY_COUNT,
-    FH_FILENAME, 
-    TO_TIMESTAMP(FH_TIMESTAMP) AS FH_TIMESTAMP
+    'Equitable' AS "COMPANY",
+        MGA,
+        "BRANCH" AS "DISTRIBUTOR CODE",
+        "AGENT" AS "AGENT SELLING CODE",
+        CASE
+            WHEN TRIM("POLICY") = 'nan' THEN NULL
+            ELSE TRIM("POLICY")
+        END AS "POLICY",
+        CASE
+            WHEN TRIM("OWNER LAST NAME") = 'nan' THEN NULL
+            ELSE TRIM("OWNER LAST NAME")
+        END AS "LAST NAME",
+        DATE("GEN DATE") AS "GENERATION DATE",
+        "DESC" AS "COMMISSION TYPE",
+        CASE
+            WHEN TRIM(CAST("PLAN CODE" AS VARCHAR)) = 'nan' THEN NULL
+            ELSE TRIM(CAST("PLAN CODE" AS VARCHAR))
+        END AS "BASIC PLAN CODE",
+        "COMMISSIONABLE PREMIUM",
+        "COMM RATE" AS "COMMISSION RATE",
+        "COMP AMT" AS "AMOUNT",
+        "COMM SHARE" AS "SHARE %",
+        NULL AS "BUSINESS FIRST NAME",  
+        CASE
+            WHEN TRIM("AGENT NAME") = 'nan' THEN NULL
+            ELSE TRIM("AGENT NAME")
+        END AS "BUSINESS SURNAME",
+        CASE
+            WHEN TRIM("DESC") IN ('Annuity FYC', 'Annuity Override', 'Life FYC', 'Life Override') THEN 'First Year'
+            WHEN TRIM("DESC") IN ('Life Renewal', 'Life Renewal Override') THEN 'Renewal'
+            WHEN TRIM("DESC") IN ('Debit Balance Recovery', 'Deductions') THEN 'Debt'
+            WHEN TRIM("DESC") IN ('Group Renewal') THEN 'Group'
+            WHEN TRIM("DESC") IN ('Misc') THEN 'Other'
+            ELSE 'N/A'
+        END AS "COMMISSION TAG",
+        "POLICY COUNT",
+        FH_FILENAME,
+        FH_TIMESTAMP
 FROM {{ source('ren_comm', 'equitable') }}
