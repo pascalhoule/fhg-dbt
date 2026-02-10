1171d4413fa98f938be987498532191d3827245b
Arzoo Mor <Arzoo.Mor@Financialhorizons.ca>
2026-02-10 22:30:10 +0000

    added policy status column in policy table

--- models/report/ops_taskreport/policy_fh_ops_taskreport.sql
+++ models/report/ops_taskreport/policy_fh_ops_taskreport.sql
@@ -6,7 +6,21 @@
         tags="ops_taskreport"			
     )			}}	
 
-SELECT
-    *
-FROM
-    {{ ref('policy_vc_clean_insurance') }}
+
+with p as (
+    select *
+    from {{ ref('policy_vc_clean_insurance') }}
+),
+s as (
+    select
+        statusvalue,
+        englishdescription as policy_status
+    from {{ ref('policystatus_vc_clean_insurance') }}
+)
+
+select
+    p.*,
+    s.policy_status
+from p
+left join s
+    on p.status = s.statusvalue
