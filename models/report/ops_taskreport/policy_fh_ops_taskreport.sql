faf60eae9e75386e0c75da60527fd9aa76de634e
Arzoo Mor <Arzoo.Mor@financialhorizons.ca>
2026-02-20 15:51:04 +0000

    changed the policy table reference for policy table in taskreport

--- models/report/ops_taskreport/policy_fh_ops_taskreport.sql
+++ models/report/ops_taskreport/policy_fh_ops_taskreport.sql
@@ -7,20 +7,7 @@
     )			}}	
 
 
-with p as (
     select *
-    from {{ ref('policy_vc_clean_insurance') }}
-),
-s as (
-    select
-        statusvalue,
-        englishdescription as policy_status
-    from {{ ref('policystatus_vc_clean_insurance') }}
-)
+    from {{ ref('policy_fh_normalize_insurance') }}
 
-select
-    p.*,
-    s.policy_status
-from p
-left join s
-    on p.status = s.statusvalue
+
